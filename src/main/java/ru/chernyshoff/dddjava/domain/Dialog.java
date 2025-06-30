117f2f595cfab0e108912dfda1b1965db2bce8c4
bearcolonel <mikhail.semchinov@gmail.com>
2025-06-30 21:50:43 +0300

    add Message

--- src/main/java/ru/chernyshoff/dddjava/domain/Dialog.java
+++ src/main/java/ru/chernyshoff/dddjava/domain/Dialog.java
@@ -1,17 +1,10 @@
 package ru.chernyshoff.dddjava.domain;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
 public class Dialog {
     private List<UUID> userIds;
     private List<Message> messages;
-
-    public static class Message {
-        private UUID userId;
-        private String text;
-        private LocalDateTime time;
-    }
 
 }
---
+++ src/main/java/ru/chernyshoff/dddjava/domain/Message.java
@@ -0,0 +1,12 @@
+package ru.chernyshoff.dddjava.domain;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class Message {
+
+    private UUID userId;
+    private String text;
+    private LocalDateTime time;
+
+}
